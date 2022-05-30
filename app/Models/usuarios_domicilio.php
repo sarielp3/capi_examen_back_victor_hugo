14c8d599f7098afba090b5cd526a16044b4f3ee6
sarielp3 <sarielp3@hotmail.com>
2022-05-30 12:47:22 -0600

    Migracion tabla user_domicilio

--- app/Models/usuarios_domicilio.php
+++ app/Models/usuarios_domicilio.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class usuarios_domicilio extends Model
 {
     use HasFactory;
+    use HasFactory;
+    protected $table = 'torneos';
+    public $timestamps = false;
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['user_id', 'domicilio','numero_exterior','colonia','cp','ciudad'];
 }
